8fa393bef9cab5bd79d50a6c01d38dfe71acc674
leticiancalvess <leticiancalves1@gmail.com>
2025-04-20 22:09:29 -0300

    validando o comportamento esperado

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -2,13 +2,21 @@ import { test, expect } from '@playwright/test'
 
 test('deve poder cadastrar uma tarefa', async ({ page, request }) => {
 
+    //Dado que eu tenho uma nova tarefa
     const taskName = 'Ler um livro de TypeScript'
     await request.delete('http://localhost:3333/helper/tasks/' + taskName)
 
+    //E que estou na página de cadastro
     await page.goto('http://localhost:8080')
 
+    //Quando faço um novo cadastro de uma tarefa
     const inputTaskName = page.locator('input[class*=InputNewTask]') // é a definição de um objeto, e não um step, então não precisa de await
     await inputTaskName.fill(taskName)
+    
     await page.click('css=button >> text=Create')
+
+    //Então a nova tarefa deverá ser apresentada na tela
+    const target = page.locator(`css=.task-item p >> text=${taskName}`)
+    await expect(target).toBeVisible()
 
 })
