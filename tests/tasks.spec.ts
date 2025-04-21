3e365740a4275c2da993d67f63907c795b1d9957
leticiancalvess <leticiancalves1@gmail.com>
2025-04-21 08:27:53 -0300

    interface TaskModel

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,28 +1,33 @@
 import { test, expect } from '@playwright/test'
 
+import { TaskModel } from './fixtures/task.model'
+
 test('deve poder cadastrar uma tarefa', async ({ page, request }) => {
 
+    const task: TaskModel = {
+        name: 'Ler um livro de TypeScript',
+        is_done: false
+    }
     //Dado que eu tenho uma nova tarefa
-    const taskName = 'Ler um livro de TypeScript'
-    await request.delete('http://localhost:3333/helper/tasks/' + taskName)
+    await request.delete('http://localhost:3333/helper/tasks/' + task.name)
 
     //E que estou na página de cadastro
     await page.goto('http://localhost:8080')
 
     //Quando faço um novo cadastro de uma tarefa
     const inputTaskName = page.locator('input[class*=InputNewTask]') // é a definição de um objeto, e não um step, então não precisa de await
-    await inputTaskName.fill(taskName)
+    await inputTaskName.fill(task.name)
     
     await page.click('css=button >> text=Create')
 
     //Então a nova tarefa deverá ser apresentada na tela
-    const target = page.locator(`css=.task-item p >> text=${taskName}`)
+    const target = page.locator(`css=.task-item p >> text=${task.name}`)
     await expect(target).toBeVisible()
 
 })
 
-test.only('não deve permitir tarefa duplicada', async ({ page, request }) => {
-    const task = {
+test('não deve permitir tarefa duplicada', async ({ page, request }) => {
+    const task: TaskModel = {
         name: 'Comprar Ketchup',
         is_done: false
     }
